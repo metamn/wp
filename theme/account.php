348c829122ce4a578d2fec2677554663f531a57c
metamn <bartus.csongor@gmail.com>
2012-06-26 09:37:09 +0300

    Small tweaks

--- theme/account.php
+++ theme/account.php
@@ -4,6 +4,7 @@
   </header>
   
   <ul>
+    <li><span>214</span> puncte Smuff</li>
     <li><span>10</span> cumparaturi</li>
     <li><span>4</span> produse in wishlist</li>
     <li><span>3</span> liste personalizate de cadouri</li>
